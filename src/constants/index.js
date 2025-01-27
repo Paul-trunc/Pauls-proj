568d810dc0010169c7bdffc7e455e24a220df29b
Paul <montebonchristian1@gmail.com>
2025-01-27 22:27:47 +0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/Gluegent.png";
+import project2 from "../assets/projects/ThingsboardIOT.png";
+import project3 from "../assets/projects/Portfolio.png";
+// import project4 from "../assets/projects/project-4/jpg";
+
+export const Intro = `I am a passionate software developer with a knack for crafting robust and scalable web applications. With 2 years and 3 months of hands-on experience, I have honed my skills in front-end technologies like React, Native Javascript and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const aboutText = `I am a dedicated and versatile software developer with a passion for creating efficient and user-friendly web applications. With 2 years and 6 months of professional experience, I have worked with a variety of technologies, including React, Node and other libraries and frameworks. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Software Developer",
+    company: "MinebeaMitsumi Inc.",
+    description: `developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React", "PostgreSQL", "PHP", "C#", "HTML", "CSS", "NODE", "Typescript"],
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "Gluegent Flow",
+    image: project1,
+    description:
+      "A flow node which directs you to next approver for form filing and automation with less coding for Javascript ",
+    technologies: ["Javascript"],
+  },
+  {
+    title: "Thingsboard IOT",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Javascript", "Jquery", "PostgreSQL"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "Tailwindcss", "Node", "Framer Motion"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bonifacio Extension Danao City ",
+  phoneNo: "+63 9097780196 ",
+  email: "montebonchristian1@gmail.com",
+};
